16c705f262459ea8019b55a6dd49142a8b483ba0
mauilema <srjorgemaciel2002@gmail.com>
2022-06-14 22:20:31 -0400

    Created a Spring server

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,29 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.*;
+
+//Controller
+//Tell Spring this is going to handle rest requests
+@RestController
+public class HelloWorldController {
+    //GET need to map the get request to the URI
+    //URI - /hello-world
+    //method- "Hello World"
+//    @RequestMapping(method= RequestMethod.GET, path = "/hello-world")
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World :)";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World :)");
+    }
+
+    //hello-world/path-variable/userInput
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+}
